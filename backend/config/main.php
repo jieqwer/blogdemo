a25314a96b10bbfaac74ffb909ff58f7c1018675
jie <2272752599@qq.com>
2018-07-05 16:30:15 +0800

    first commit

---
+++ backend/config/main.php
@@ -0,0 +1,43 @@
+<?php
+$params = array_merge(
+    require(__DIR__ . '/../../common/config/params.php'),
+    require(__DIR__ . '/../../common/config/params-local.php'),
+    require(__DIR__ . '/params.php'),
+    require(__DIR__ . '/params-local.php')
+);
+
+return [
+    'id' => 'app-backend',
+    'basePath' => dirname(__DIR__),
+    'controllerNamespace' => 'backend\controllers',
+    'bootstrap' => ['log'],
+    'modules' => [],
+	'language'=> 'zh-CN',
+    'components' => [
+        'user' => [
+            'identityClass' => 'common\models\Adminuser',
+            'enableAutoLogin' => true,
+        ],
+    	'session'=>[
+    			'name'=>'PHPBACKSESSION',
+    			'savePath'=>sys_get_temp_dir(),
+    	],
+    	'request'=>[
+    			'cookieValidationKey'=>'sdfjjksloeedf78789judf',
+    			'csrfParam'=>'_adminCSRF',
+    	],
+        'log' => [
+            'traceLevel' => YII_DEBUG ? 3 : 0,
+            'targets' => [
+                [
+                    'class' => 'yii\log\FileTarget',
+                    'levels' => ['error', 'warning'],
+                ],
+            ],
+        ],
+        'errorHandler' => [
+            'errorAction' => 'site/error',
+        ],        
+    ],
+    'params' => $params,
+];
